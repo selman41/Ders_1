7bca3725be3061229411932d16a8a0b1e3ff9260
selma <selma@SELMAN_YILDIRIM>
2023-05-10 20:02:43 +0300

    Proje dosyası ekle.

---
+++ Ders_1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp64
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0;                                                  //  a değişkenini sıfıra atadık
+
+            if (a > 0)                                                  // bir koşul oluşturduk ve bu koşula a sıfırdan büyükse dedik 
+            {
+                Console.WriteLine("a pozitif bir tam sayıdır");
+            }
+            else if (a < 0)                                             // burda da a 'nın sıfırdan küçük olma durumunu ele aldık
+            {
+                Console.WriteLine(" a negatif bir tam sayıdır ");
+            }
+            else                                                        // burda da a 'nın sıfıra eşit oldugu durumu ele aldık
+            {
+                Console.WriteLine("a sayası sıfırdır");
+            }
+            Console.ReadKey();
+        }
+    }
+}
